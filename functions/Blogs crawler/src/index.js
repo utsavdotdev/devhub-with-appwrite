0a401e48445103f8a9db38e09da5f2ec5c1ecf6d
utsavbhattarai007 <ninja08469@gmail.com>
2023-06-07 19:57:30 +0545

    blog crawler configured and hosted

--- functions/Blogs crawler/src/index.js
+++ functions/Blogs crawler/src/index.js
@@ -1,30 +1,9 @@
-// import { Client, Account, Databases } from "node-appwrite";
-// import axios from "axios";
-// import moment from "moment";
-// import {} from "dotenv/config";
-// import { v4 as uuidv4 } from "uuid";
-// import { Query } from "appwrite";
-//change the above code to common js
 const { Client, Account, Databases, Query } = require("node-appwrite");
 const axios = require("axios");
 const moment = require("moment");
 require("dotenv/config");
 const { v4: uuidv4 } = require("uuid");
-
-
-
-//Initialize the client SDK
-const client = new Client();
-
-client.setEndpoint(process.env.APPWRITE_ENDPOINT); // API Endpoint
-client.setProject(process.env.APPWRITE_PROJECT_ID); // Project ID
-client.setKey(process.env.APPWRITE_API_KEY); // API Key for the project
-
-//Initialize the account
-const account = new Account(client);
-
-//Initialize the database
-const database = new Databases(client, process.env.APPWRITE_DATABASE_ID);
+const fetch = require("node-fetch");
 
 //Function to fetch blogs
 const fetchBlogs = async () => {
@@ -67,9 +46,7 @@ const SaveBlogsToDB = async (blogs) => {
     const blogExists = await database.listDocuments(
       process.env.APPWRITE_DATABASE_ID,
       process.env.APPWRITE_BLOGS_COLLECTION_ID,
-      [
-        Query.equal("id", blogData.id),
-      ]
+      [Query.equal("id", blogData.id)]
     );
 
     if (blogExists.documents.length > 0) {
@@ -82,25 +59,84 @@ const SaveBlogsToDB = async (blogs) => {
         process.env.APPWRITE_DATABASE_ID,
         process.env.APPWRITE_BLOGS_COLLECTION_ID,
         uuidv4(),
-        blogData,
+        blogData
       );
-      console.log("Blog saved:", doc.$id);
     } catch (error) {
       console.log("Failed to save Blog:", error);
     }
   }
 };
 
-// Main function to fetch blogs from Hashnode and save to Appwrite
-const fetchAndSaveBlogs = async () => {
+module.exports = async function (req, res) {
+  //Initialize the client SDK
+  const client = new Client();
+
+  client.setEndpoint(req.variables.APPWRITE_ENDPOINT); // API Endpoint
+  client.setProject(req.variables.APPWRITE_PROJECT_ID); // Project ID
+  client.setKey(req.variables.APPWRITE_API_KEY); // API Key for the project
+
+  //Initialize the account
+  const account = new Account(client);
+
+  //Initialize the database
+  const database = new Databases(client, req.variables.APPWRITE_DATABASE_ID);
+
   try {
-    const blogs = await fetchBlogs();
-    await SaveBlogsToDB(blogs);
-    console.log("Blogs saved successfully!");
+    const blogs = await fetch(`https://hashnode.com/api/feed/featured?page=1`);
+    const response = await blogs.json();
+    const filteredResponse = response.posts.filter(
+      (post) => post.coverImage !== ""
+    );
+
+    if (filteredResponse.length === 0) {
+      return res.json({
+        message: "No new blogs found",
+      });
+    }
+    for (const blog of filteredResponse) {
+      const blogData = {
+        id: blog?._id,
+        title: blog?.title,
+        image: blog?.coverImage,
+        date: new Date(blog?.dateAdded).toDateString(),
+        read_time: blog?.readTime,
+        source: "hashnode",
+        createdAt: moment().format("MMMM Do YYYY, h:mm:ss a"),
+        user_name: blog?.author?.name,
+        user_image: blog?.author?.photo,
+        url: !blog?.author?.publicationDomain
+          ? `https://${blog?.author?.username}.hashnode.dev/${blog?.slug}`
+          : `https://${blog?.author?.publicationDomain}/${blog?.slug}`,
+        timestamp: Date.now(),
+      };
+
+      //filter out blogs that are already saved
+      const blogExists = await database.listDocuments(
+        req.variables.APPWRITE_DATABASE_ID,
+        req.variables.APPWRITE_BLOGS_COLLECTION_ID,
+        [Query.equal("id", blogData.id)]
+      );
+
+      if (blogExists?.documents?.length == 0) {
+        const doc = await database.createDocument(
+          req.variables.APPWRITE_DATABASE_ID,
+          req.variables.APPWRITE_BLOGS_COLLECTION_ID,
+          uuidv4(),
+          blogData
+        );
+        res.json({
+          message: "Blogs saved successfully:" + " " + doc.$id,
+        });
+      }
+    }
+    res.json({
+      message: "Action completed successfully",
+    });
   } catch (error) {
-    console.error("Failed to fetch and save blogs:", error);
+    console.log("Failed to fetch and save blogs:", error);
+    res.json({
+      message: "Failed to fetch and save blogs",
+      error: error.toString(),
+    });
   }
 };
-
-// Call the main function
-fetchAndSaveBlogs();
