110c280c3eacc99a8b5b2203c1ae6867d2e3cf11
utsavbhattarai007 <ninja08469@gmail.com>
2023-06-11 13:50:06 +0545

    demo account feature created

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -5,19 +5,45 @@ import { TextField } from "@pankod/refine-mui";
 import { Navigate, useNavigate } from "react-router-dom";
 import { SiCodemagic } from "react-icons/si";
 import { account } from "../appwrite/appwriteConfig";
+import { ID } from "appwrite";
 import { toast } from "react-hot-toast";
 import { ContextProvider } from "../config/Context";
 import { v4 as uuidv4 } from "uuid";
 
 const Auth = () => {
-  const { userDetails } = useContext(ContextProvider);
-  const [user, setuser] = userDetails;
   const token = localStorage.getItem("token");
   const url = import.meta.env.VITE_URL;
 
+  const [demo, setdemo] = React.useState({
+    email: "",
+    password: "",
+  });
   if (token) {
     return <Navigate to="/app" replace />;
   }
+
+  const demoSignin = async (e) => {
+    e.preventDefault();
+    if (!demo.email || !demo.password)
+      return toast.error("Please enter email and password");
+    const check = confirm(
+      "This is for demo account. Normal signin is done through Google."
+    );
+    if (!check) return;
+    try {
+      const res = await account.createEmailSession(demo.email, demo.password);
+      if (res) {
+        const user = await account.getPrefs();
+        localStorage.setItem("token", user.token);
+        window.location.reload();
+      }
+    } catch (e) {
+      if (e.code === 401) {
+        return toast.error(e.message);
+      }
+      toast.error("Something went wrong!");
+    }
+  };
 
   const signIn = (e) => {
     e.preventDefault();
@@ -44,49 +70,55 @@ const Auth = () => {
           <p>Please enter your details</p>
         </header>
         <form className={style.form_auth}>
-          <TextField
-            InputLabelProps={{
-              style: { fontFamily: "Poppins", fontSize: "15px" },
-            }}
-            sx={{
-              fontFamily: "Poppins",
-            }}
-            id="outlined-basic"
-            label="Username"
-            variant="outlined"
-            disabled={true}
-          />
-          <TextField
-            InputLabelProps={{
-              style: { fontFamily: "Poppins", fontSize: "15px" },
-            }}
-            sx={{
-              fontFamily: "Poppins",
-            }}
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            disabled={true}
-          />
-          <Button
-            onClick={() => {
-              alert("Please continue with google.");
-            }}
-            sx={{
-              fontWeight: 500,
-              color: "#fff",
-              fontFamily: "Poppins",
-              textTransform: "capitalize",
-              padding: "7px 0",
-            }}
-            disableElevation
-            className={style.login_btn}
-            variant="contained"
-            disabled={true}
-          >
-            Continue
-          </Button>
-          {/* // generate or divider with css */}
+          <div className={style.form_wrapper}>
+            <TextField
+              InputLabelProps={{
+                style: { fontFamily: "Poppins", fontSize: "15px" },
+              }}
+              sx={{
+                fontFamily: "Poppins",
+              }}
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              value={demo.email}
+              name="email"
+              onChange={(e) => {
+                setdemo({ ...demo, [e.target.name]: e.target.value });
+              }}
+            />
+            <TextField
+              InputLabelProps={{
+                style: { fontFamily: "Poppins", fontSize: "15px" },
+              }}
+              sx={{
+                fontFamily: "Poppins",
+              }}
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              value={demo.password}
+              name="password"
+              onChange={(e) => {
+                setdemo({ ...demo, [e.target.name]: e.target.value });
+              }}
+            />
+            <Button
+              onClick={(e) => demoSignin(e)}
+              sx={{
+                fontWeight: 500,
+                color: "#fff",
+                fontFamily: "Poppins",
+                textTransform: "capitalize",
+                padding: "7px 0",
+              }}
+              disableElevation
+              className={style.login_btn}
+              variant="contained"
+            >
+              Continue
+            </Button>
+          </div>
           <div className={style.divider}>
             <div className={style.line}></div>
             <p>or</p>
